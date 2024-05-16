f956443a5588e5c27947e10fd89d8da36191ef89
skichula <157534429+skichula@users.noreply.github.com>
2024-05-16 06:46:36 -0400

    Add files via upload

---
+++ chall2.js
@@ -0,0 +1,13 @@
+function copyAddress() {
+    var billing = document.querySelector("#bill")
+    var is_checked = document.querySelector("#sameAddress").checked
+    var home = document.querySelector("#home")
+    console.log(is_checked)
+    if (is_checked == true) {
+        home.value = billing.value;
+        home.disabled = true;
+    } else  if (is_checked == false) {
+        home.value = ""; 
+        home.disabled = false;
+    }
+}
